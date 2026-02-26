836c363726b774d2de57216b683abbc7c1770da3
Lite <valyaskorobagatko@gmail.comgit config --global user.email valyaskorobagatko@gmail.com>
2026-02-26 14:59:09 +0200

    add tast-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,9 @@
 function getShippingMessage(country, price, deliveryFee) {
 
-}
+    const totalPrice = price + deliveryFee;
 
-const shippingMessage = getShippingMessage(country, price, deliveryFee);
+return `Shipping to ${country} will cost ${totalPrice} credits`;  
+}
 
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
