ab76027a5e1adc179846733e4857223f25e5432c
Lite <valyaskorobagatko@gmail.comgit config --global user.email valyaskorobagatko@gmail.com>
2026-02-27 08:11:42 +0200

    implement getElementWidth function

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,17 @@
 function getElementWidth(content, padding, border) {
 
+    const numContent = Number.parseInt(content);
+    const numPadding = Number.parseInt(padding);
+    const numBorder = Number.parseFloat(border);
+
+    const totalWeight = numContent + (2 * (numPadding + numBorder));
+
+    return totalWeight;
+    
+       
 }
-
-const elementWidth = getElementWidth(content, padding, border);
-
+ 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+ 
